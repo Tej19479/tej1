700c00b53e6a286fe2d68e8e340976bac318d616
Tej Pratap <Tej Pratap@LAPTOP-A2U1Q2JU>
2021-10-01 14:51:54 +0530

    hello

---
+++ src/test/java/demo/test1.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+		@Test
+		public void tc1()
+		{
+			Reporter.log("hello seleium ",true);
+		}
+}
---
+++ src/test/java/demo/test2.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test2 {
+		@Test
+		public void tc1()
+		{
+			Reporter.log("hello pytrhion ",true);
+		}
+}
